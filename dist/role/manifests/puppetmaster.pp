7d08ca5d90e2ee2005f6e79f686b3801ef7d79b4
Guillaume Lederrey <guillaume.lederrey@gmail.com>
2013-09-26 20:44:02 +0200

    fixed alt_names

--- dist/role/manifests/puppetmaster.pp
+++ dist/role/manifests/puppetmaster.pp
@@ -3,7 +3,7 @@ class role::puppetmaster {
   class { 'puppet':
     mode          => 'server',
     server        => 'puppet.ledcom.ch',
-    dns_alt_names => [ 'puppet.ledcom.ch' ],
+    dns_alt_names => 'puppet.ledcom.ch',
     environment   => 'production',
     manifest_path => '\$confdir/environments/$environment/site/site.pp',
     module_path   => '/etc/puppet/environments/$environment/modules:/etc/puppet/environments/$environment/dist',
