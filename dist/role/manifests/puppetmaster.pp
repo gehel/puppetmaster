e855f012b008d508550fd8efabe6f4859fa2c6b2
Guillaume Lederrey <guillaume.lederrey@gmail.com>
2013-09-29 08:45:06 +0200

    added basic hiera support

--- dist/role/manifests/puppetmaster.pp
+++ dist/role/manifests/puppetmaster.pp
@@ -5,13 +5,27 @@ class role::puppetmaster {
     server        => 'puppet.ledcom.ch',
     dns_alt_names => 'puppet.ledcom.ch',
     environment   => 'production',
-    manifest_path => '\$confdir/environments/$environment/site/site.pp',
+    manifest_path => '$confdir/environments/$environment/site/site.pp',
     module_path   => '/etc/puppet/environments/$environment/modules:/etc/puppet/environments/$environment/dist',
+  }
+
+  package { 'hiera-eyaml':
+    ensure   => 'latest',
+    provider => 'gem',
+  } -> file { '/etc/puppet/hiera.yaml':
+    ensure => 'present',
+    source => 'puppet:///modules/role/hiera.yaml',
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0644',
   }
 
   class { 'puppi': }
 
-  cron { 'r10k-deploy-env':
+  package { 'r10k':
+    ensure   => 'latest',
+    provider => 'gem',
+  } -> cron { 'r10k-deploy-env':
     command => '/usr/local/bin/r10k deploy environment -p',
     minute  => '*/5',
   }
