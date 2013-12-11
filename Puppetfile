6b6db2b6c2e38b67ba060af6e4e36c681be22e2c
Guillaume Lederrey <guillaume.lederrey@gmail.com>
2013-12-11 23:07:54 +0100

    added basic workstation definition

--- Puppetfile
+++ Puppetfile
@@ -1,32 +1,50 @@
+mod 'apache',
+  :git => 'https://github.com/example42/puppet-apache.git'
+
+mod 'apt',
+  :git => 'https://github.com/example42/puppet-apt.git'
+
+mod 'cntlm',
+  :git => 'https://github.com/gehel/puppet-cntlm.git'
+
+mod 'collectd',
+  :git => 'https://github.com/example42/puppet-collectd.git'
+
+mod 'concat',
+  :git => 'https://github.com/puppetlabs/puppetlabs-concat.git'
+
+mod 'ddclient',
+  :git => 'https://github.com/example42/puppet-ddclient.git'
+
 mod 'dns',
   :git => 'https://github.com/masterzen/puppet-dns.git'
 
-mod 'puppi',
-  :git => 'https://github.com/example42/puppi.git'
+mod 'dnsmasq',
+  :git => 'https://github.com/example42/puppet-dnsmasq.git'
 
 mod 'firewall',
   :git => 'https://github.com/example42/puppet-firewall.git'
 
-mod 'monitor',
-  :git => 'https://github.com/example42/puppet-monitor.git'
+mod 'git',
+  :git => 'https://github.com/example42/puppet-git.git'
 
-mod 'nrpe',
-  :git => 'https://github.com/example42/puppet-nrpe.git'
+mod 'icinga',
+  :git => 'https://github.com/example42/puppet-icinga.git'
 
-mod 'openssh',
-  :git => 'https://github.com/example42/puppet-openssh.git'
+mod 'iptables',
+  :git => 'https://github.com/example42/puppet-iptables.git'
 
-mod 'rsyslog',
-  :git => 'https://github.com/example42/puppet-rsyslog.git'
+mod 'java',
+  :git => 'https://github.com/example42/puppet-java.git'
 
-mod 'apache',
-  :git => 'https://github.com/example42/puppet-apache.git'
+mod 'logrotate',
+  :git => 'https://github.com/example42/puppet-logrotate.git'
 
 mod 'mcollective',
   :git => 'https://github.com/example42/puppet-mcollective.git'
 
-mod 'nginx',
-  :git => 'https://github.com/example42/puppet-nginx.git'
+mod 'monitor',
+  :git => 'https://github.com/example42/puppet-monitor.git'
 
 mod 'munin',
   :git => 'https://github.com/example42/puppet-munin.git'
@@ -34,55 +52,54 @@ mod 'munin',
 mod 'mysql',
   :git => 'https://github.com/example42/puppet-mysql.git'
 
-mod 'puppet',
-  :git => 'https://github.com/example42/puppet-puppet.git'
-
 mod 'nagios',
   :git => 'https://github.com/example42/puppet-nagios.git'
+
+mod 'nginx',
+  :git => 'https://github.com/example42/puppet-nginx.git'
+
+mod 'nrpe',
+  :git => 'https://github.com/example42/puppet-nrpe.git'
+
+mod 'ntp',
+  :git => 'https://github.com/example42/puppet-ntp.git'
+
+mod 'openssh',
+  :git => 'https://github.com/example42/puppet-openssh.git'
 
 mod 'profile',
   :git => 'https://github.com/example42/puppet-profile.git'
 
-mod 'logrotate',
-  :git => 'https://github.com/example42/puppet-logrotate.git'
+mod 'puppet',
+  :git => 'https://github.com/example42/puppet-puppet.git'
 
-mod 'apt',
-  :git => 'https://github.com/example42/puppet-apt.git'
+mod 'puppi',
+  :git => 'https://github.com/example42/puppi.git'
 
-mod 'concat',
-  :git => 'https://github.com/puppetlabs/puppetlabs-concat.git'
+mod 'rsyslog',
+  :git => 'https://github.com/example42/puppet-rsyslog.git'
 
-mod 'java',
-  :git => 'https://github.com/example42/puppet-java.git'
-
-mod 'icinga',
-  :git => 'https://github.com/example42/puppet-icinga.git'
-
-mod 'sudo',
-  :git => 'https://github.com/example42/puppet-sudo.git'
-
-mod 'wget',
-  :git => 'https://github.com/example42/puppet-wget.git'
-
-mod 'iptables',
-  :git => 'https://github.com/example42/puppet-iptables.git'
-
-mod 'tomcat',
-  :git => 'https://github.com/example42/puppet-tomcat.git'
-
-mod 'dnsmasq',
-  :git => 'https://github.com/example42/puppet-dnsmasq.git'
-
-mod 'varnish',
-  :git => 'https://github.com/example42/puppet-varnish.git'
-
-mod 'ddclient',
-  :git => 'https://github.com/example42/puppet-ddclient.git'
-
-mod 'collectd',
-  :git => 'https://github.com/example42/puppet-collectd.git'
+mod 'rvm',
+  :git => 'https://github.com/example42/puppet-rvm.git'
 
 mod 'stdlib',
   :git => 'https://github.com/puppetlabs/puppetlabs-stdlib.git'
 
+mod 'sudo',
+  :git => 'https://github.com/example42/puppet-sudo.git'
+
+mod 'svn',
+  :git => 'https://github.com/example42/puppet-svn.git'
+
+mod 'tomcat',
+  :git => 'https://github.com/example42/puppet-tomcat.git'
+
+mod 'vagrant',
+  :git => 'https://github.com/example42/puppet-vagrant.git'
+
+mod 'varnish',
+  :git => 'https://github.com/example42/puppet-varnish.git'
+
+mod 'wget',
+  :git => 'https://github.com/example42/puppet-wget.git'
 
---
+++ dist/role/manifests/dev_workstation.pp
@@ -0,0 +1,144 @@
+# sudo ln -s /home/gehel/dev/puppet/puppetmaster/hiera.yaml /etc/puppet/
+# sudo puppet apply \
+#      --modulepath='/home/gehel/dev/puppet/puppetmaster/dist:/home/gehel/dev/puppet/puppetmaster/modules' \
+#      -e "include role::dev_workstation"
+class role::dev_workstation {
+  $monitor = true
+  $monitor_tool = ['puppi']
+  $puppi = true
+
+  package { [
+    'chromium-browser',
+    'cmus',
+    'dos2unix',
+    'emacs',
+    'htop',
+    'maven2',
+    'openjdk-7-jdk',
+    'secure-delete',
+    'synergy',
+    'telnet',
+    'unity-tweak-tool',
+    'virtualbox',
+    ]:
+    ensure => 'present',
+  }
+
+  package { ['hiera-eyaml', 'r10k',]:
+    ensure   => 'present',
+    provider => 'gem',
+  }
+
+  class { 'git':
+  }
+
+  class { 'openssh':
+  }
+
+  class { 'puppi':
+  }
+
+  class { 'svn':
+  }
+
+  package { 'ruby-dev': ensure => 'present', } -> class { 'vagrant': }
+
+  class { 'cntlm': }
+
+  class { 'ntp': }
+
+  Exec {
+    logoutput => 'on_failure', }
+
+  exec { 'download-STS':
+    path    => '/usr/bin',
+    command => 'wget -cv http://download.springsource.com/release/STS/3.4.0/dist/e4.3/spring-tool-suite-3.4.0.RELEASE-e4.3.1-linux-gtk-x86_64.tar.gz',
+    cwd     => '/tmp',
+    creates => '/tmp/spring-tool-suite-3.4.0.RELEASE-e4.3.1-linux-gtk-x86_64.tar.gz',
+    unless  => 'test -d /opt/springsource/sts-3.4.0.RELEASE',
+  } -> exec { 'untar-STS':
+    path    => '/bin',
+    command => 'tar xzvf /tmp/spring-tool-suite-3.4.0.RELEASE-e4.3.1-linux-gtk-x86_64.tar.gz',
+    cwd     => '/opt',
+    creates => '/opt/springsource/sts-3.4.0.RELEASE',
+  }
+
+  exec { 'download-Geppetto':
+    path    => '/usr/bin',
+    command => 'wget -cv https://downloads.puppetlabs.com/geppetto/geppetto-linux.gtk.x86_64-3.2.0-R201307041307.zip',
+    cwd     => '/tmp',
+    creates => '/tmp/geppetto-linux.gtk.x86_64-3.2.0-R201307041307.zip',
+    unless  => 'test -d /opt/geppetto',
+  } -> exec { 'untar-Geppetto':
+    path    => '/usr/bin',
+    command => 'unzip /tmp/geppetto-linux.gtk.x86_64-3.2.0-R201307041307.zip',
+    cwd     => '/opt',
+    creates => '/opt/geppetto',
+  }
+
+  exec { 'download-IntelliJ':
+    path    => '/usr/bin',
+    command => 'wget -cv http://download-ln.jetbrains.com/idea/ideaIU-12.1.6.tar.gz',
+    cwd     => '/tmp',
+    timeout => '900',
+    creates => '/tmp/ideaIU-12.1.6.tar.gz',
+    unless  => 'test -d /opt/idea-IU-129.1359',
+  } -> exec { 'untar-IntelliJ':
+    path    => '/usr/bin:/bin',
+    command => 'tar xzvf /tmp/ideaIU-12.1.6.tar.gz',
+    cwd     => '/opt',
+    creates => '/opt/idea-IU-129.1359',
+  }
+
+  # inotify needs to be higher than default for IntelliJ to be happy
+  file { '/etc/sysctl.d/99-inotify.conf':
+    ensure  => 'present',
+    content => "fs.inotify.max_user_watches = 524288\n",
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+  }
+
+  file { '/home/gehel/.homesick':
+    ensure => 'directory',
+    owner  => 'gehel',
+    group  => 'gehel',
+  } -> file { '/home/gehel/.homesick/repos':
+    ensure => 'directory',
+    owner  => 'gehel',
+    group  => 'gehel',
+  } -> exec { 'checkout-Homeshick':
+    path    => '/usr/bin',
+    command => 'git clone https://github.com/andsens/homeshick.git /home/gehel/.homesick/repos/homeshick',
+    creates => '/home/gehel/.homesick/repos/homeshick',
+  }
+
+  # Source code pro font
+  file { '/usr/share/fonts/truetype/SourceCodePro':
+    ensure => 'directory',
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0755',
+  } -> exec { 'download-source-code-pro':
+    path    => '/usr/bin',
+    command => 'wget -cv http://optimate.dl.sourceforge.net/project/sourcecodepro.adobe/SourceCodePro_FontsOnly-1.017.zip',
+    cwd     => '/tmp',
+    creates => '/tmp/SourceCodePro_FontsOnly-1.017.zip',
+    unless  => 'test -f /usr/share/fonts/truetype/SourceCodePro-Regular.ttf',
+  } -> exec { 'unzip-source-code-pro':
+    path    => '/usr/bin',
+    command => 'unzip /tmp/SourceCodePro_FontsOnly-1.017.zip',
+    cwd     => '/tmp',
+    creates => '/tmp/SourceCodePro_FontsOnly-1.017',
+    unless  => 'test -f /usr/share/fonts/truetype/SourceCodePro-Regular.ttf',
+  } -> exec { 'deploy-source-code-pro':
+    path    => '/usr/bin:/bin',
+    command => 'cp /tmp/SourceCodePro_FontsOnly-1.017/TTF/*.ttf /usr/share/fonts/truetype',
+    cwd     => '/tmp',
+    creates => '/usr/share/fonts/truetype/SourceCodePro-Regular.ttf',
+  } ~> exec { 'refresh-font-cache':
+    path        => '/usr/bin',
+    command     => 'fc-cache',
+    refreshonly => true,
+  }
+}
