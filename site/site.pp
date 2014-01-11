f87e17b18d61655c630323d4fa27fcdae79843cb
Guillaume Lederrey <guillaume.lederrey@gmail.com>
2014-01-11 12:13:06 +0100

    Now deploy OpenSSH module by default

--- site/site.pp
+++ site/site.pp
@@ -1,11 +1,15 @@
 case $server_role {
-  'puppetmaster': { class { 'role::puppetmaster': } }
+  'puppetmaster' : {
+    class { 'role::puppetmaster': }
+  }
 }
 
 node 'galadriel.home.ledcom.ch' {
   class { 'puppet': }
+
   class { 'role::dev_workstation': }
 }
 
 node default {
+  class { 'openssh': }
 }
