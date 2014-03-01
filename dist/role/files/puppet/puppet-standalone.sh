0c87fd8b157bd8248158da779f2498a3a48b0981
gehel <guillaume.lederrey@gmail.com>
2014-03-01 09:53:34 +0100

    run r10k before puppet agent

--- dist/role/files/puppet/puppet-standalone.sh
+++ dist/role/files/puppet/puppet-standalone.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+/usr/local/bin/r10k -v info deploy environment -p
+
 puppet apply \
   --modulepath=/etc/puppet/environments/production/modules:/etc/puppet/environments/production/dist \
   --hiera_config=/etc/puppet/environments/production/hiera.yaml \
