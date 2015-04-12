c5c8d3c4178bd8b516529c241e0fc04b9cc8dbfd
gehel <guillaume.lederrey@gmail.com>
2015-04-12 10:20:49 +0200

    better way of running puppet

--- dist/role/manifests/firewall.pp
+++ dist/role/manifests/firewall.pp
@@ -1,11 +1,13 @@
 class role::firewall inherits role::default {
+
+  $puppet_fqdn = 'voyage.home.ledcom.ch'
   
   file { '/usr/local/sbin/puppet-standalone.sh':
-    ensure => 'present',
-    source => 'puppet:///modules/role/puppet/puppet-standalone.sh',
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0775',
+    ensure  => 'present',
+    content => template('role/puppet/puppet-standalone.sh.erb'),
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0775',
   } ->
   cron { 'puppet-standalone':
     command => '/usr/local/sbin/puppet-standalone.sh',
