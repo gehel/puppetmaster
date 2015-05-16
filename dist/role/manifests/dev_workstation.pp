093622d7571d674b9f315dc2626f4ec642d467c5
Guillaume Lederrey <gehel@galadriel.home.ledcom.ch>
2015-05-16 22:31:53 +0200

    cleanup

--- dist/role/manifests/dev_workstation.pp
+++ dist/role/manifests/dev_workstation.pp
@@ -80,44 +80,30 @@ class role::dev_workstation inherits role::default {
     version => '3.0.5',
   }
 
-  exec { 'download-STS':
-    path    => '/usr/bin',
-    command => 'wget -cv http://download.springsource.com/release/STS/3.4.0/dist/e4.3/spring-tool-suite-3.4.0.RELEASE-e4.3.1-linux-gtk-x86_64.tar.gz',
-    cwd     => '/tmp',
-    timeout => '900',
-    creates => '/tmp/spring-tool-suite-3.4.0.RELEASE-e4.3.1-linux-gtk-x86_64.tar.gz',
-    unless  => 'test -d /opt/springsource/sts-3.4.0.RELEASE',
-  } -> exec { 'untar-STS':
-    path    => '/bin',
-    command => 'tar xzvf /tmp/spring-tool-suite-3.4.0.RELEASE-e4.3.1-linux-gtk-x86_64.tar.gz',
-    cwd     => '/opt',
-    creates => '/opt/springsource/sts-3.4.0.RELEASE',
-  }
-
   exec { 'download-Geppetto':
     path    => '/usr/bin',
-    command => 'wget -cv https://downloads.puppetlabs.com/geppetto/4.x/geppetto-linux.gtk.x86_64-4.0.0-R201310140657.zip',
+    command => 'wget -cv https://downloads.puppetlabs.com/geppetto/4.x/geppetto-linux.gtk.x86_64-4.3.1-R201501182354.zip',
     cwd     => '/tmp',
     timeout => '900',
-    creates => '/tmp/geppetto-linux.gtk.x86_64-4.0.0-R201310140657.zip',
+    creates => '/tmp/geppetto-linux.gtk.x86_64-4.3.1-R201501182354.zip',
     unless  => 'test -d /opt/geppetto',
   } -> exec { 'untar-Geppetto':
     path    => '/usr/bin',
-    command => 'unzip /tmp/geppetto-linux.gtk.x86_64-4.0.0-R201310140657.zip',
+    command => 'unzip /tmp/geppetto-linux.gtk.x86_64-4.3.1-R201501182354.zip',
     cwd     => '/opt',
     creates => '/opt/geppetto',
   }
 
   exec { 'download-IntelliJ':
     path    => '/usr/bin',
-    command => 'wget -cv http://download-ln.jetbrains.com/idea/ideaIU-13.0.2.tar.gz',
+    command => 'wget -cv http://download-ln.jetbrains.com/idea/ideaIU-14.1.3.tar.gz',
     cwd     => '/tmp',
     timeout => '900',
-    creates => '/tmp/ideaIU-13.0.2.tar.gz',
-    unless  => 'test -d /opt/idea-IU-133.696',
+    creates => '/tmp/ideaIU-14.1.3.tar.gz',
+    unless  => 'test -d /opt/idea-*',
   } -> exec { 'untar-IntelliJ':
     path    => '/usr/bin:/bin',
-    command => 'tar xzvf /tmp/ideaIU-13.0.2.tar.gz',
+    command => 'tar xzvf /tmp/ideaIU-14.1.3.tar.gz',
     cwd     => '/opt',
     creates => '/opt/idea-133.696',
   }
@@ -128,21 +114,6 @@ class role::dev_workstation inherits role::default {
   sysctl::value { 'fs.inotify.max_user_watches':
     value => '524288',
   }
-
-  file { '/home/gehel/.homesick':
-    ensure => 'directory',
-    owner  => 'gehel',
-    group  => 'gehel',
-  } -> file { '/home/gehel/.homesick/repos':
-    ensure => 'directory',
-    owner  => 'gehel',
-    group  => 'gehel',
-  } -> exec { 'checkout-Homeshick':
-    path    => '/usr/bin',
-    command => 'git clone https://github.com/andsens/homeshick.git /home/gehel/.homesick/repos/homeshick',
-    creates => '/home/gehel/.homesick/repos/homeshick',
-  }
-
   # Source code pro font
   file { '/usr/share/fonts/truetype/SourceCodePro':
     ensure => 'directory',
@@ -178,66 +149,9 @@ class role::dev_workstation inherits role::default {
     monthday => "*",
   }
   
-  # eyaml keys
-  file { '/home/gehel/eyaml':
-    ensure => 'directory',
-    owner  => 'gehel',
-    group  => 'gehel',
-    mode   => '0775',
-  }
-  file { '/home/gehel/eyaml/private_key.pkcs7.pem':
-    ensure  => 'present',
-    content => hiera('eyaml_private_key'),
-    owner   => 'gehel',
-    group   => 'gehel',
-    mode    => '0600',
-  }
-  file { '/home/gehel/eyaml/public_key.pkcs7.pem':
-    ensure => 'present',
-    source => 'puppet:///modules/role/eyaml/public_key.pkcs7.pem',
-    owner  => 'gehel',
-    group  => 'gehel',
-    mode   => '0664',
-  }
-
-  # SSH keys
-  file { '/home/gehel/.ssh/backup':
-    ensure  => 'present',
-    content => hiera('ssh_gehel_backup_private_key'),    
-    owner   => 'gehel',
-    group   => 'gehel',
-    mode    => '0600',
-  }
-  file { '/home/gehel/.ssh/backup.pub':
-    ensure => 'present',
-    source => 'puppet:///modules/role/gehel/ssh/backup.pub',
-    owner  => 'gehel',
-    group  => 'gehel',
-    mode   => '0644',
-  }
-  file { '/home/gehel/.ssh/id_dsa':
-    ensure  => 'present',
-    content => hiera('ssh_gehel_private_key'),    
-    owner   => 'gehel',
-    group   => 'gehel',
-    mode    => '0600',
-  }
-  file { '/home/gehel/.ssh/id_dsa.pub':
-    ensure => 'present',
-    source => 'puppet:///modules/role/gehel/ssh/id_dsa.pub',
-    owner  => 'gehel',
-    group  => 'gehel',
-    mode   => '0644',
-  }
-  
   # basic dirs
   file { [
     '/home/gehel/dev',
-    '/home/gehel/dev/aws',
-    '/home/gehel/dev/ohi',
-    '/home/gehel/dev/JavaDojo',    
-    '/home/gehel/dev/puppet',
-    '/home/gehel/dev/utilities',
   ]:
     ensure => 'directory',
     owner  => 'gehel',
